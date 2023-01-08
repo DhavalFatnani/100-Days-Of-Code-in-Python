a15db998a456c5b1869bcf30cb99d89af223f890
Dhaval Fatnani <Dhavalfatnani017@gmail.com>
2023-01-08 23:07:41 +0530

    Day3 Excerise4 Completed(Pizza Automatic Order)

--- Day 3/day3_concepts.py
+++ Day 3/day3_concepts.py
@@ -1,14 +1,26 @@
 print("Welcome to the roller coaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the roller coaster!")
     age = int(input("What is you age?"))
     if age < 12:
-        print("Please pay Rs.50")
+        bill = 50
+        print("Child tickets are Rs.50")
     elif age <= 18:
-        print("Please pay Rs.80")
+        bill = 80
+        print("Youth tickets are Rs.80")
     else:
-        print("Please pay Rs.140")
+        bill = 140
+        print("Adults tickets are Rs.140")
+
+    wants_photo = input("Do you want a photo taken? Y or N.")
+    wants_photo = wants_photo.lower()
+    if wants_photo == 'y':
+        #Add 50rs. to the bill
+        bill += 50
+
+        print(f"Your final bill is rs.{bill}")
 else:
     print("Sorry, you have to grow taller before you can ride.")
