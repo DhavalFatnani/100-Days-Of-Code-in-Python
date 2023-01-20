d7535dce5dfe21e1f2195be73c90f741afc1ba0d
Dhaval Fatnani <Dhavalfatnani017@gmail.com>
2023-01-21 02:10:42 +0530

    Day9 Excercise2 Completed

--- Day 9/day9_concepts.py
+++ Day 9/day9_concepts.py
@@ -23,3 +23,42 @@ print(programming_dictionary)
 for key in programming_dictionary:
     print(key)
     print(programming_dictionary[key])
+
+# NESTING
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+#Nesting a List in a Dictionary
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+
+travel = {
+    "France": {
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+    "Germany": {
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5
+    }
+}
+
+#Nesting Dictionary in a list
+
+travel_list = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits":12
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits":5
+    }
+]
