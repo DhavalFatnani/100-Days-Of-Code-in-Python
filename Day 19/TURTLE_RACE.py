b088e9191820a801de72720ad29b809ab81d87a8
Dhaval Fatnani <Dhavalfatnani017@gmail.com>
2023-06-01 22:50:55 +0530

    Day 20 Completed

--- Day 19/TURTLE_RACE.py
+++ Day 19/TURTLE_RACE.py
@@ -3,7 +3,7 @@ import random
 
 is_race_on = False
 screen = Screen()
-screen.setup(width=500, height=400)
+screen.setup(width=600, height=450)
 user_bet = screen.textinput(title="Make your Bet", prompt="Which turtle will win the race? Enter its color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 turtles = []
@@ -13,10 +13,18 @@ y = 100
 _y = 30
 miku = Turtle()
 miku.penup()
+miku.goto(x=-210, y=150)
+miku.seth(270)
+miku.pendown()
+miku.write("Start Line")
+miku.forward(275)
+miku.penup()
 miku.goto(x=230, y=150)
+miku.write("Finish Line")
 miku.seth(270)
 miku.pendown()
 miku.forward(275)
+miku.penup()
 miku.hideturtle()
 
 for color in colors:
@@ -30,16 +38,28 @@ for color in colors:
 if user_bet:
     is_race_on = True
 
-while is_race_on:
-    for turtle in turtles:
-        if turtle.xcor() > 230:
-            is_race_on = False
-            winning_turtle = turtle.pencolor()
-            if winning_turtle == user_bet:
-                print(f"You're Correct! You Won, The Winning turtle is {winning_turtle}.")
-            else:
-                print(f"You're Wrong! You Lose, The Winning turtle is {winning_turtle}.")
-        randomDistance = random.randint(0, 10)
-        turtle.forward(randomDistance)
+miku.goto(200, 180)
+miku.write("Winner: ")
+
+
+def race():
+    global is_race_on
+    while is_race_on:
+        for turtle in turtles:
+            if turtle.xcor() > 230:
+                is_race_on = False
+                winning_turtle = turtle.pencolor()
+                miku.goto(240, 180)
+                miku.write(winning_turtle)
+                if winning_turtle == user_bet:
+                    print(f"You're Correct! You Won, The Winning turtle is {winning_turtle}.")
+                else:
+                    print(f"You're Wrong! You Lose, The Winning turtle is {winning_turtle}.")
+            randomDistance = random.randint(0, 10)
+            turtle.forward(randomDistance)
+
+
+screen.listen()
+screen.onkey(key="space", fun=race)
 
 screen.exitonclick()
