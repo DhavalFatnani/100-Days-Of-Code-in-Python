b6584a4e799efa3d6d8961ade0e7401dbc319318
Dhaval Fatnani <Dhavalfatnani017@gmail.com>
2023-06-05 11:35:37 +0530

    Day 25, Excercise 1, Completed

---
+++ Day 26/day26_concepts.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3]
+new_numbers = [n + 1 for n in numbers]
+name = "Dhaval"
+letter_list = [letter for letter in name]
+numbers_list = [1, 2, 3, 4]
+manipulated_list = [n*2 for n in range(1, 5)]
+names = ["Dhaval", "Hasti", "Sahil", "Dev", "Neil"]
+short_names = [name for name in names if len(name) < 5]
+capital_long_names = [name.upper() for name in names if len(name)>4]
